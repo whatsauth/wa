0c63d1942c668dd85b14cf31be77cd8cd7d49f9f
Rolly Maulana Awangga <awangga@gmail.com>
2023-11-11 06:51:05 +0700

    upgrade

--- store.go
+++ store.go
@@ -1,20 +1,15 @@
 package wa
 
 import (
-	"fmt"
-
 	"go.mau.fi/whatsmeow/store/sqlstore"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func GetWaClient(phonenumber string, client []*WaClient, mongoconn *mongo.Database, container *sqlstore.Container) (waclient WaClient, err error) {
 	id := WithPhoneNumber(phonenumber, client)
-	fmt.Println("id array:", id)
 	if id >= 0 {
-		fmt.Println("ada di array:", id)
 		waclient = *client[id]
 	} else {
-		fmt.Println("masuk ke container whatsmeow")
 		waclient, err = ClientDB(phonenumber, mongoconn, container)
 		client = append(client, &waclient)
 	}
