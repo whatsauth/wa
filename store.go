33322ec425ea6bf4042396f922f23ef890f44ec1
Rolly Maulana Awangga <awangga@gmail.com>
2023-11-11 12:22:15 +0700

    WithPhoneNumber pakai db

--- store.go
+++ store.go
@@ -1,12 +1,14 @@
 package wa
 
 import (
+	"github.com/aiteung/atdb"
 	"go.mau.fi/whatsmeow/store/sqlstore"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func GetWaClient(phonenumber string, client []*WaClient, mongoconn *mongo.Database, container *sqlstore.Container) (waclient *WaClient, err error) {
-	id := WithPhoneNumber(phonenumber, client)
+	id, err := WithPhoneNumber(phonenumber, client, mongoconn)
 	if id >= 0 {
 		waclient = client[id]
 	} else {
@@ -16,11 +18,13 @@ func GetWaClient(phonenumber string, client []*WaClient, mongoconn *mongo.Databa
 	return
 }
 
-func WithPhoneNumber(phonenumber string, clients []*WaClient) int {
+func WithPhoneNumber(phonenumber string, clients []*WaClient, mongoconn *mongo.Database) (idx int, err error) {
+	user, err := atdb.GetOneLatestDoc[User](mongoconn, "user", bson.M{"phonenumber": phonenumber})
+	idx = -1
 	for i, client := range clients {
-		if client.PhoneNumber == phonenumber {
-			return i
+		if (client.ID.User == phonenumber) && (client.ID.Device == user.DeviceID) {
+			idx = i
 		}
 	}
-	return -1
+	return
 }
