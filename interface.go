4c8c97de39b7193011cfeef7f07007b6bb725e0a
JPratama7 <josepratama080@gmail.com>
2023-11-20 20:48:47 +0700

    feat(interface): interface baru

--- interface.go
+++ interface.go
@@ -4,3 +4,12 @@ type StoreClient interface {
 	StoreClient(id string, client *WaClient)
 	StoreOnlineClient(id string, client *WaClient) (ok bool)
 }
+
+type GetClient interface {
+	GetClient(id string) (client *WaClient, ok bool)
+}
+
+type GetStoreClient interface {
+	StoreClient
+	GetClient
+}
